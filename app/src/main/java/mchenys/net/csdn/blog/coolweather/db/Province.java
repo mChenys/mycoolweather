c1286223eda3e8a17f934715d8018f64a9c4c08c
mChenys <mChenys>
2016-12-16 16:10:57 +0800

    create database by litepal

---
+++ app/src/main/java/mchenys/net/csdn/blog/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package mchenys.net.csdn.blog.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 省份实体类
+ * Created by mChenys on 2016/12/16.
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
